6c7436f7dbe4bb304e81cbded3785e7a240beff0
Alex Berry <alexander.e.berry@hotmail.co.uk>
2017-05-29 12:13:35 +0100

    Shark type Carnivore

--- Animals/Shark.java
+++ Animals/Shark.java
@@ -1,30 +1,36 @@
-// package animals;
-// import foods.Meat;
-// import java.util.*;
-//
-// public class Shark extends Animal{
-//
-//   private String name;
-//   private ArrayList<Meat> belly;
-//
-//   public Shark(String name) {
-//     this.name = name;
-//     this.belly = new ArrayList<Meat>();
-//   }
-//
-//   public String getName() {
-//     return this.name;
-//   }
-//
-//   public int countFood() {
-//     return belly.size();
-//   }
-//
-//   public void eat(Meat meat) {
-//     belly.add(meat);
-//   }
-//
-//   public void sleep() {
-//     belly.clear();
-//   }
-// }
+package animals;
+import foods.Meat;
+import java.util.*;
+
+public class Shark extends Animal{
+
+  private String name;
+  private ArrayList<Meat> belly;
+  private AnimalType type;
+
+  public Shark(String name) {
+    this.name = name;
+    this.belly = new ArrayList<Meat>();
+    this.type = AnimalType.CARNIVORE;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public AnimalType getType() {
+    return this.type;
+  }
+
+  public int countFood() {
+    return belly.size();
+  }
+
+  public void eat(Meat meat) {
+    belly.add(meat);
+  }
+
+  public void sleep() {
+    belly.clear();
+  }
+}
--- Specs/SharkTest.java
+++ Specs/SharkTest.java
@@ -1,33 +1,39 @@
-// import static org.junit.Assert.*;
-// import org.junit.Test;
-// import animals.*;
-// import foods.Meat;
-//
-// public class SharkTest {
-//
-//   @Test
-//   public void canGetName() {
-//     Shark shark = new Shark("Gregory");
-//     assertEquals("Gregory", shark.getName());
-//   }
-//
-//   @Test
-//   public void canEat() {
-//     Shark shark = new Shark("Gregory");
-//     Meat meat = new Meat();
-//     shark.eat(meat);
-//     assertEquals(1, shark.countFood());
-//   }
-//
-//   @Test
-//   public void emptyBellyAfterSleep() {
-//     Shark shark = new Shark("Gregory");
-//     Meat meat = new Meat();
-//     shark.eat(meat);
-//     shark.sleep();
-//     assertEquals(0, shark.countFood());
-//   }
-//
-//
-//
-// }
+import static org.junit.Assert.*;
+import org.junit.Test;
+import animals.*;
+import foods.Meat;
+
+public class SharkTest {
+
+  @Test
+  public void canGetName() {
+    Shark shark = new Shark("Gregory");
+    assertEquals("Gregory", shark.getName());
+  }
+
+  @Test
+  public void canGetType() {
+    Shark shark = new Shark("Gregory");
+    assertEquals(AnimalType.CARNIVORE, shark.getType());
+  }
+
+  @Test
+  public void canEat() {
+    Shark shark = new Shark("Gregory");
+    Meat meat = new Meat();
+    shark.eat(meat);
+    assertEquals(1, shark.countFood());
+  }
+
+  @Test
+  public void emptyBellyAfterSleep() {
+    Shark shark = new Shark("Gregory");
+    Meat meat = new Meat();
+    shark.eat(meat);
+    shark.sleep();
+    assertEquals(0, shark.countFood());
+  }
+
+
+
+}
